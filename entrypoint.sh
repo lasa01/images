4c5859386f28b4a779cc00dd916f9f976d2e291a
Lassi Säike <lassisaike11@gmail.com>
2019-04-09 14:04:56 +0300

    Update entrypoint.sh
    Added support for ARK mods

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Update Source Server
 if [ ! -z ${SRCDS_APPID} ]; then
+    echo "Updating server"
     if [ ! -z ${SRCDS_BETAID} ]; then
         if [ ! -z ${SRCDS_BETAPASS} ]; then
             ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
@@ -17,12 +18,18 @@ if [ ! -z ${SRCDS_APPID} ]; then
     fi
     # Update mods
     if [ ! -z ${SRCDS_MODS} && ! -z ${SRCDS_MODAPPID} ]; then
+        echo "Updating mods"
         UPDATE_MODS="./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container"
         for val in ${SRCDS_MODS}; do
             UPDATE_MODS="$UPDATE_MODS +workshop_download_item ${SRCDS_MODAPPID} $val"
         done
         UPDATE_MODS="$UPDATE_MODS +quit"
         eval $UPDATE_MODS
+        
+        # Link mods to correct directory for ARK
+        for val in ${SRCDS_MODS}; do
+            ln -nsf /home/container/steamapps/workshop/content/${SRCDS_MODAPPID}/$val /home/container/ShooterGame/Content/Mods/$val
+        done
     fi
 fi
 
