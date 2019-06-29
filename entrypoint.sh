9f4d28ed807198fdc10a6ca153aeb8fb3c536b02
Lassi Säike <lassisaike11@gmail.com>
2019-06-29 23:55:47 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,5 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
+cd /home/container/DedicatedServer64
 eval ${MODIFIED_STARTUP}
