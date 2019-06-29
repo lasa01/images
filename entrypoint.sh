7e38f0e5f60c35660f29ecc204c9fc06096915ae
Lassi Säike <lassisaike11@gmail.com>
2019-06-29 16:05:41 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,26 +4,9 @@ sleep 1
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-# Update Source Server
-if [ ! -z ${SRCDS_APPID} ]; then
-    if [ ! -z ${SRCDS_BETAID} ]; then
-        if [ ! -z ${SRCDS_BETAPASS} ]; then
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
-        else
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
-        fi
-    else
-        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
-    fi
-    # Update mods
-    if [ ! -z ${SRCDS_MODS} && ! -z ${SRCDS_MODAPPID} ]; then
-        UPDATE_MODS="./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container"
-        for val in ${SRCDS_MODS}; do
-            UPDATE_MODS="$UPDATE_MODS +workshop_download_item ${SRCDS_MODAPPID} $val"
-        done
-        UPDATE_MODS="$UPDATE_MODS +quit"
-        eval $UPDATE_MODS
-    fi
+# Update Server
+if [ ! -z ${STEAM_APPID} ]; then
+    ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${STEAM_APPID} +quit
 fi
 
 # Replace Startup Variables
